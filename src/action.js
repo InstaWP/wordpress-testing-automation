5cbdd4beb53e6d6feeca93aefb01060fb4611093
Vikas Singhal <vikas.programmer@gmail.com>
2022-04-04 16:00:54 +0530

    PR thingy

--- src/action.js
+++ src/action.js
@@ -1,3 +1,20 @@
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
+const octokit = github.getOctokit(GITHUB_TOKEN);
+
+const { context = {} } = github;
+const { pull_request } = context.payload;
+
+await octokit.issues.createComment({
+  ...context.repo,
+  issue_number: pull_request.number,
+  body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
+});
+
 async function run() {
   console.log('Hello, world!');
 }
